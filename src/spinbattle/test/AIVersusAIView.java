74c69618e3ae9d2074e98afb8e7607acbcfbdc2e
Castaras <Castaraspie@googlemail.com>
2018-06-07 16:02:41 +0100

    AIVersusAIView
    class that plays two ai against each other until one wins. was going to have options for choosing ai but lack of options in simpleplayerinterface means it's not possible in the timeframe.

---
+++ src/spinbattle/test/AIVersusAIView.java
@@ -0,0 +1,34 @@
+package spinbattle.test;
+
+import ggi.core.SimplePlayerInterface;
+import spinbattle.core.SpinGameState;
+import spinbattle.params.Constants;
+import spinbattle.params.SpinBattleParams;
+import spinbattle.players.HeuristicLauncher;
+import spinbattle.view.SpinBattleView;
+import utilities.JEasyFrame;
+
+public class AIVersusAIView {
+
+    public static void main(String[] args) throws Exception {
+        SpinBattleParams params = new SpinBattleParams();
+        SpinGameState gameState = new SpinGameState().setParams(params).setPlanets();
+        SpinBattleView view = new SpinBattleView().setParams(params).setGameState(gameState);
+        
+        
+        HeuristicLauncher player1 = new HeuristicLauncher(); // change one of these
+        HeuristicLauncher player2 = new HeuristicLauncher(); // to make it play against a new ai
+        // just make sure the new ai has a makeTransits(state, constants) method.
+        
+        new JEasyFrame(view, "Spin Battle Game");
+        while(!gameState.gameOver()) {
+            gameState.next(null);
+            gameState = (SpinGameState) gameState.copy();
+            player1.makeTransits(gameState, Constants.playerOne);
+            player2.makeTransits(gameState, Constants.playerTwo);
+            view.setGameState((SpinGameState) gameState.copy());
+            view.repaint();
+            Thread.sleep(20);
+        }
+    }
+}
